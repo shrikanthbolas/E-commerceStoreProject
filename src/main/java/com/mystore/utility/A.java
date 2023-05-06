1a17242e751cbd5d843e1c9bee364d5406bef150
shrikanthbolas <krshrikanth@gmail.com>
2023-05-06 20:02:55 +0545

    my first commit to git store project

---
+++ src/main/java/com/mystore/utility/A.java
@@ -0,0 +1,12 @@
+package com.mystore.utility;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generatfsdfjksldjfjed method stub
+		System.out.println("shri hari");
+		System.out.println("shri hari");  
+		System.out.println("shri hari1");
+	}
+
+}
